ce8a34418cf23e13c98aa5632885ebfba935bcc1
xCharlotte <charlottevoskuilen@hotmail.com>
2018-01-24 18:19:25 +0100

    Added a blog controller for the front-end

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class BlogController extends Controller
+{
+  public function index(Post $post) {
+    $posts = Post::latest()->paginate(5);
+    return view('blog.index')->with(['posts'=>$posts]);
+  }
+}
