886f453b02657b50831c01718263f15a59eb91f9
xCharlotte <charlottevoskuilen@hotmail.com>
2018-01-27 13:54:54 +0100

    Changed the show view for backend

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,69 +2,38 @@
 
 @section('content')
 <div class="container">
-  <div id="scrolltop"></div>
     <div class="row">
         <div class="col-md-16">
             <div class="panel panel-default">
-                <div class="panel-heading"><h1>{{ $posts->title }}</h1>
-                <h5>Geschreven door {{ $posts->creator->name }} </h5></div>
+                <div class="panel-heading"><h1>Show</h1></div>
                 <div class="panel-body">
-                  <article>
-                    <div class="body">
-                      <img src="/images/{{ $posts->image }}"/>
-                      {!! $posts->body !!}
+                  <form method="post" action="posts/show/{{ $post['id'] }}">
+                    {{csrf_field()}}
+                    <div class="row">
+                      <div class="col-md-4"></div>
+                      <div class="form-group col-md-4">
+                        <label for="name">Title:</label>
+                        <input type="text" class="form-control" name="name" value="{{$post->title}}">
+                      </div>
                     </div>
-                  </article>
-                </div>
-            </div>
-
-            <div class="row">
-              <div class="col-md-12">
-                @foreach($posts->comment as $comment)
-                  <div class="panel panel-default">
-                    <div class="panel-heading">
-                      {{ $comment->name }} gaf een reactie
-                      {{ $comment->created_at->diffForHumans() }}</div>
-                    <div class="panel-body">
-                      <article>
-                        <div class="body">{{ $comment->body }}</div>
-                      </article>
+                    <div class="row">
+                      <div class="col-md-4"></div>
+                        <div class="form-group col-md-4">
+                          <label for="price">Body:</label>
+                          <input type="text" class="form-control" name="price" value="{{$post->body}}">
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                @endforeach
-              </div>
+                    <div class="row">
+                      <div class="col-md-4"></div>
+                      <div class="form-group col-md-4">
+                        <button type="submit" class="btn btn-success">Sla op!</button>
+                      </div>
+                    </div>
+                  </form>
             </div>
-
-            <div class="row">
-              <div class="col-md-12">
-                <form method="POST" action="{{ route('addcomment',$posts->id) }}">
-                  {{ csrf_field() }}
-
-                  <div class="form-group">
-                    <input name="name" id="name" class="form-control" placeholder="Naam" required>
-                    <textarea name="body" id="body" class="form-control" placeholder="Laat een bericht achter!" rows="5" required></textarea>
-                  </div>
-
-                  <button type="submit" class="btn btn-default">Verstuur</button>
-                </form>
-              </div>
-            </div>
-          </div>
         </div>
     </div>
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function (){
-    $("#click").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#scrolltop").offset().top
-        }, 2000);
-    });
-});
-</script>
-  <a href="#" id="click"class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-arrow-up"></span>Naar Boven</a>
 </div>
 
 @endsection
