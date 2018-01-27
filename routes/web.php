028c061249cdb0bf976afc0f8c13b06a3e3d4880
xCharlotte <charlottevoskuilen@hotmail.com>
2018-01-27 13:52:04 +0100

    Added a route for the show of post on front end

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('post', 'PostController');
 # Front-end
 Route::get('/', 'BlogController@index');
+Route::get('/{post}', 'BlogController@show');
 Route::post('{post}/comment','CommentController@store')->name('addcomment');
 
 # Back-end
 Route::get('/posts', 'PostController@index')->middleware('auth');
-Route::get('post/create','PostController@create')->middleware('auth');
-Route::get('/post/{post}/edit', 'PostController@edit')->middleware('auth');
-Route::get('/post/{post}', 'PostController@show');
-Route::post('post/{post}/edit','PostController@update');
-Route::post('post/create','PostController@store');
-Route::delete('/post/{id}', 'PostController@destroy')->middleware('auth');
+Route::get('posts/create','PostController@create')->middleware('auth');
+Route::get('/posts/{post}/edit', 'PostController@edit')->middleware('auth');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('posts/{post}/edit','PostController@update');
+Route::post('posts/create','PostController@store');
+Route::delete('/posts/{id}', 'PostController@destroy')->middleware('auth');
 
 Auth::routes();
 
